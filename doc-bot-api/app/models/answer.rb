b065856bb2b4d99b69101f646e8a02822fa88269
Matt Bogen <zebogen@gmail.com>
2017-05-13 16:33:51 -0500

    Link answer and consultation

--- doc-bot-api/app/models/answer.rb
+++ doc-bot-api/app/models/answer.rb
@@ -1,2 +1,3 @@
 class Answer < ApplicationRecord
+  belongs_to :consultation
 end
--- doc-bot-api/db/migrate/20170513200003_create_answers.rb
+++ doc-bot-api/db/migrate/20170513200003_create_answers.rb
@@ -1,9 +1,12 @@
 class CreateAnswers < ActiveRecord::Migration[5.1]
   def change
     create_table :answers do |t|
+      t.integer :consultation_id
       t.string :value
 
       t.timestamps
+
+      t.index :consultation_id
     end
   end
 end
