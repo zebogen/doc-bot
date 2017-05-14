06de1b37b5ce1d3beee2c4ac017bcea68163db46
Matt Bogen <zebogen@gmail.com>
2017-05-14 02:12:39 -0500

    Refactor recommendation class

--- doc-bot-api/app/models/recommendation.rb
+++ doc-bot-api/app/models/recommendation.rb
@@ -6,28 +6,33 @@ class Recommendation
     @size = size
   end
 
+  SORT_FUNCTIONS_BY_SLUG = {
+    'doctor-location' => ->(a, b) { b.distance <=> a.distance },
+    'yelp-ratings' => ->(a, b) { a.yelp_rating.to_f <=> b.yelp_rating.to_f },
+    'experience' => ->(a, b) { a.last_degree_year.to_i <=> b.last_degree_year.to_i }
+  }
+
   def generate
-    factors = @consultation.recommendation_factors
-    sorted_lists = factors.each_with_object({}) do |factor, sorted_lists|
-      case factor
-      when 'doctor-location'
-        sorted_lists[factor] = doctors_for_complaint.sort { |a, b| b.distance <=> a.distance }
-      when 'yelp-ratings'
-        sorted_lists[factor] = doctors_for_complaint.sort { |a, b| a.yelp_rating.to_f <=> b.yelp_rating.to_f }
-      when 'experience'
-        sorted_lists[factor] = doctors_for_complaint.sort { |a, b| a.last_degree_year.to_i <=> b.last_degree_year.to_i }
-      end
-    end.compact
     doctors_for_complaint.sort_by do |doc|
       gender_score = doc.gender && @consultation.gender_preference == doc.gender ? doctors_for_complaint.length : 0
-      location_score = sorted_lists.key?('doctor-location') ? sorted_lists['doctor-location'].find_index { |d| d.id == doc.id } : 0
-      ratings_score = sorted_lists.key?('yelp-ratings') ? sorted_lists['yelp-ratings'].find_index { |d| d.id == doc.id } : 0
-      experience_score = sorted_lists.key?('experience') ? sorted_lists['experience'].find_index { |d| d.id == doc.id } : 0
-      gender_score + location_score + ratings_score + experience_score
+      gender_score + sort_score(doc, 'doctor-location') + sort_score(doc, 'yelp-ratings') + sort_score(doc, 'experience')
     end.last(@size)
   end
 
   private
+
+  def sort_score(doc, slug)
+    sorted_lists.key?(slug) ? sorted_lists[slug].find_index { |d| d.id == doc.id } : 0
+  end
+
+  def sorted_lists
+    @sorted_lists ||= begin
+      @consultation.recommendation_factors.each_with_object({}) do |factor, sorted_lists|
+        next unless SORT_FUNCTIONS_BY_SLUG.key?(factor)
+        sorted_lists[factor] = doctors_for_complaint.sort(&SORT_FUNCTIONS_BY_SLUG[factor])
+      end.compact
+    end
+  end
 
   def doctors_for_complaint
     @doctors_for_complaint ||= begin
