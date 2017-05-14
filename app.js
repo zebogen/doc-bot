8f6269d149d938346e2659be48db11db7dbb591d
Feargal Walsh <feargswalsh@gmail.com>
2017-05-13 20:41:36 -0500

    Added yelp fusion npm package

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const http = require('http')
 const Bot = require('messenger-bot')
 
 let bot = new Bot({
-  token: process.env.PAGE_TOKEN,
-  verify: process.env.VERIFY_TOKEN
+  token: 'EAAAAAJADyRMBABHZAcunPJPJK3TFJu0vAbyu2oqkwh9kgIy09lJevkRu713lCo9dQpQ7MwAtupc4b5PfnmezNeoJcPS4hv7fcMZAYQtxiyOSsk64Sm1W1UieXAZBWGPEgSv0OozAPsdBZA62aTu4hmLwnfPE4sMdfPCZCYh3ZAZBgZDZD',
+  verify: "VERIFY_TOKEN"
 });
 
 bot.on('error', (err) => {
@@ -15,6 +15,14 @@ bot.on('message', (payload, reply) => {
   console.log(`message received: ${payload}`)
   let text = payload.message.text
 
+
+
+// bot.on('message',(payload,reply) => {
+//   console.log('messge received: $ {payload}')
+//   lat = event.message.attachments[0].payload.coordinates.lat
+//   lng = event.message.attachments[0].payload.coordinates.long
+// )
+// })
   bot.getProfile(payload.sender.id, (err, profile) => {
     if (err) throw err
 
